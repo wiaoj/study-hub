adc7222ae2316d28b26fae72db63c9543747f442
wiaoj <0.wiaoj@gmail.com>
2024-03-16 08:14:21 +0300

    backend: graphql <memi lavi>

---
+++ backend/graphql/memi-lavi-grphql/dotnet/GraphQl/GraphQl/Query.cs
@@ -0,0 +1,29 @@
+﻿using GraphQl.Data;
+using GraphQl.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace GraphQl.GraphQl;
+public class Query {
+    public Task<List<Book>> Books(ClaimsPrincipal claims, ApplicationDbContext context, String? title) {
+        DbSet<Book> books = context.Books;
+        //throw new Exception("Error retrieving data");
+        return title is not null
+            ? books.Where(x => x.Title.Contains(title, StringComparison.InvariantCultureIgnoreCase)).ToListAsync()
+            : books.ToListAsync();
+    }
+}
+
+public class BookType : ObjectType<Book> {
+    protected override void Configure(IObjectTypeDescriptor<Book> descriptor) {
+        descriptor
+            .Field(x => x.Title)
+            .ResolveWith<Resolvers>(x => x.GetTitle(default!))
+            .Type<StringType>(); 
+        
+        descriptor
+            .Field(x => x.Published)
+            .ResolveWith<Resolvers>(x => x.GetFormattedDate(default!))
+            .Type<StringType>();
+    }
+}
---
+++ backend/graphql/memi-lavi-grphql/dotnet/GraphQl/GraphQl/Resolvers.cs
@@ -0,0 +1,12 @@
+﻿using GraphQl.Models;
+
+namespace GraphQl.GraphQl;
+public class Resolvers {
+    public String GetFormattedDate([Parent] Book book) {
+        return book.Published.ToShortDateString();
+    }
+
+    public String GetTitle([Parent] Book book) {
+        return $"[{book.Title.ToUpper()}]";
+    }
+}
